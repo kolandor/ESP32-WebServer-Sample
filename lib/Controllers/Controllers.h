2741cd1e5ee23cb25616b535c3158a410ff0d038
alexey.anipchenko <anipchenko@mindwave.studio>
2021-04-29 18:31:32 +0300

    Add web server Controllers

--- lib/Controllers/Controllers.h
+++ lib/Controllers/Controllers.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <WebServer.h>
 
 WebServer server(80);
@@ -5,15 +6,118 @@ WebServer server(80);
 uint8_t LED1pin = 15;
 bool LED1status = LOW;
 
-uint8_t LED2pin = 7;
+uint8_t LED2pin = 2;
 bool LED2status = LOW;
 
-String SendHTML(uint8_t led1stat,uint8_t led2stat);
-void handle_OnConnect();
-void handle_led1on();
-void handle_led1off();
-void handle_led2on();
-void handle_led2off();
-void handle_NotFound();
-void initial_controllers();
-void handle_web_clients();
+String SendHTML(uint8_t led1stat,uint8_t led2stat)
+{
+  String ptr = "<!DOCTYPE html> <html>\n";
+  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr +="<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n";
+  ptr +="<title>LED Control</title>\n";
+  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
+  ptr +=".button {display: block;width: 80px;background-color: #1abc9c;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
+  ptr +=".button-on {background-color: #FF0000;}\n";
+  ptr +=".button-on:active {background-color: #8F0000;}\n";
+  ptr +=".button-off {background-color: #34495e;}\n";
+  ptr +=".button-off:active {background-color: #2c3e50;}\n";
+  ptr +="a:last-child.button-on {background-color: gold;}\n";
+  ptr +="a:last-child.button-on:active {background-color: darkorange;}\n";
+  ptr +="p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
+  ptr +="</style>\n";
+  ptr +="</head>\n";
+  ptr +="<body>\n";
+  ptr +="<h1>Привет! Это я маленький компьютер на столе)</h1>\n";
+  ptr +="<h3>А внизу ты можешь помигать моими светодиодами</h3>\n";
+  
+  if(led1stat)
+    ptr +="<p>LED1 Status: ON</p><a class=\"button button-on\" href=\"/led1off\">OFF</a>\n";
+  else
+    ptr +="<p>LED1 Status: OFF</p><a class=\"button button-off\" href=\"/led1on\">ON</a>\n";
+
+  if(led2stat)
+    ptr +="<p>LED2 Status: ON</p><a class=\"button button-on\" href=\"/led2off\">OFF</a>\n";
+  else
+    ptr +="<p>LED2 Status: OFF</p><a class=\"button button-off\" href=\"/led2on\">ON</a>\n";
+
+  ptr +="</body>\n";
+  ptr +="</html>\n";
+  return ptr;
+}
+
+void handle_OnConnect()
+{
+  Serial.print("GPIO7 Status: ");
+  if(LED1status)
+    Serial.print("ON");
+  else
+    Serial.print("OFF");
+
+  Serial.print(" | GPIO6 Status: ");
+  if(LED2status)
+    Serial.print("ON");
+  else
+    Serial.print("OFF");
+
+  Serial.println("");
+  server.send(200, "text/html", SendHTML(LED1status,LED2status)); 
+}
+
+void handle_led1on() 
+{
+  LED1status = HIGH;
+  Serial.println("GPIO7 Status: ON");
+  server.send(200, "text/html", SendHTML(true,LED2status)); 
+}
+
+void handle_led1off() 
+{
+  LED1status = LOW;
+  Serial.println("GPIO7 Status: OFF");
+  server.send(200, "text/html", SendHTML(false,LED2status)); 
+}
+
+void handle_led2on() 
+{
+  LED2status = HIGH;
+  Serial.println("GPIO6 Status: ON");
+  server.send(200, "text/html", SendHTML(LED1status,true)); 
+}
+
+void handle_led2off() 
+{
+  LED2status = LOW;
+  Serial.println("GPIO6 Status: OFF");
+  server.send(200, "text/html", SendHTML(LED1status,false)); 
+}
+
+void handle_NotFound()
+{
+  server.send(404, "text/plain", "Not found");
+}
+
+void initial_controllers()
+{
+  server.on("/", handle_OnConnect);
+  server.on("/led1on", handle_led1on);
+  server.on("/led1off", handle_led1off);
+  server.on("/led2on", handle_led2on);
+  server.on("/led2off", handle_led2off);
+  server.onNotFound(handle_NotFound);
+}
+
+void handle_web_clients()
+{
+  server.handleClient();
+  
+  if(LED1status)
+    digitalWrite(LED1pin, HIGH);
+  else
+    digitalWrite(LED1pin, LOW);
+  
+  if(LED2status)
+    digitalWrite(LED2pin, HIGH);
+  else
+    digitalWrite(LED2pin, LOW);
+}
